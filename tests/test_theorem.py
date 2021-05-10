fb991b138e3e55b33097b1775d3535836d7ab2b3
gram <mail@orsinium.dev>
2021-05-10 15:17:56 +0200

    test Proof.__str__

--- tests/test_theorem.py
+++ tests/test_theorem.py
@@ -1,5 +1,9 @@
+import pytest
+
 # project
 from deal_solver import Theorem
+
+from .helpers import prove_f
 
 
 def test_extract_function():
@@ -32,3 +36,16 @@ def test_ignore_method():
     """
     theorems = list(Theorem.from_text(text))
     assert len(theorems) == 0
+
+
+@pytest.mark.parametrize('expr, expected', [
+    ('def f(): 13', 'nothing to prove'),
+    ('def f(): assert 1', 'assertion'),
+    ('def f(): raise ValueError', 'ValueError'),
+    ('def f(): raise ValueError("hello")', 'ValueError'),
+    ('def f(x: int): assert x != 12', 'assertion. Example: x=12.'),
+    ('def f(): zz', 'failed to interpret statement (cannot resolve name zz)'),
+])
+def test_proof_str(expr, expected):
+    proof = prove_f(expr)
+    assert str(proof) == expected
