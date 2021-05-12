e47b6fb7e6a01b671691164c85bca5277e0ddfb1
gram <mail@orsinium.dev>
2021-05-12 15:54:50 +0200

    decouple int tests

---
+++ tests/test_types/test_int.py
@@ -0,0 +1,67 @@
+import pytest
+from deal_solver import Conclusion
+from ..helpers import prove_f
+
+
+@pytest.mark.parametrize('check', [
+    # math
+    '13 == 13',
+    '+12 == 12',
+    '12 == --12',
+    '-13 == -13',
+    '+13 == --13',
+    '3 + 6 == 9',
+    '7 - 4 == 3',
+    '7 * 4 == 28',
+    '12 / 5 == 2.4',
+    '13 // 5 == 2',
+    '-13 // 5 == -3',
+    '13 // -5 == -3',
+    '13 % 5 == 3',
+    '-5 % 2 == 1',
+    '-1 % 3 == 2',
+    '1 % -3 == -2',
+    '5 % -2 == -1',
+    '2 ** 3 == 8',
+
+    # bitwise math
+    '2 | 4 == 6',
+    '3 & 6 == 2',
+    '3 ^ 6 == 5',
+    '6 << 1 == 12',
+    '6 >> 1 == 3',
+    '~3 == -4',
+    '~-6 == 5',
+    '~0 == -1',
+
+    # functions
+    'bool(0) == False',
+    'bool(1) == True',
+    'bool(14) == True',
+    'bool(-23) == True',
+    'abs(12) == 12',
+    'abs(-13) == 13',
+    'min(13, 5) == 5',
+    'min(5, 13) == 5',
+    'max(13, 5) == 13',
+    'max(5, 13) == 13',
+    'float(4) == 4.0',
+    'str(42) == "42"',
+
+    # methods
+    '(12).conjugate() == 12',
+    '(12).real == 12',
+    '(12).numerator == 12',
+    '(12).denominator == 1',
+    '(12).imag == 0',
+])
+def test_expr_asserts_ok(check: str) -> None:
+    assert eval(check)
+    text = """
+        from typing import List
+        def f():
+            assert {}
+    """
+    text = text.format(check)
+    theorem = prove_f(text)
+    assert theorem.conclusion is Conclusion.OK
