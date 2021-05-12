88bfd9c670495c3627a84c27876898a7f67f6ff7
gram <mail@orsinium.dev>
2021-05-12 10:35:38 +0200

    group tests

--- tests/test_types/test_list.py
+++ tests/test_types/test_list.py
@@ -1,5 +1,63 @@
+import pytest
 from deal_solver import Conclusion
 from ..helpers import prove_f
+
+
+@pytest.mark.parametrize('check', [
+    # compare
+    '[1, 2, 3] == [1, 2, 3]',
+    '[1, 2, 3] != [1, 2, 3, 3]',
+
+    # contains
+    '10 in [3, 6, 10, 17]',
+    '11 not in [3, 6, 10, 17]',
+    'not "" in [3]',
+
+    # index
+    '[4, 5, 6][1] == 5',
+    '[4, 5, 6, 7, 8][2:4] == [6, 7]',
+    '[4, 5, 6, 7, 8][2:] == [6, 7, 8]',
+    '[4, 5, 6, 7, 8][:4] == [4, 5, 6, 7]',
+    '[1, 2, 3][:] == [1, 2, 3]',
+
+    # operations
+    '[1] + [2] == [1, 2]',
+    '[1, 2] + [3, 4] == [1, 2, 3, 4]',
+    '[] + [] == []',
+    # '[1] * 3 == [1, 1, 1]',
+    '([] + [True] + [False])[0] == True',
+
+    # methods
+    '[7, 8, 8, 9].index(8) == 1',
+    '[7, 8, 8, 9].index(8, 2) == 2',
+    '[1, 2, 4, 6, 5, 6].count(6) == 2',
+    '[1, 1, 1].count(1) == 3',
+    '[1, 1, 1].count(2) == 0',
+    '[].count(2) == 0',
+    '[1, 2, 3].copy() == [1, 2, 3]',
+
+    # functions
+    'len([7, 9, 9, 9, 11]) == 5',
+    'min([7, 3, 5]) == 3',
+    'max([3, 7, 5]) == 7',
+    'sum([3, 7, 5]) == 15',
+    'sum([sum([3, 7]), 5]) == 15',
+
+    # implicit bool
+    'not []',
+    '[1]',
+    '[1, 2]',
+])
+def test_expr_asserts_ok(prefer_real: bool, check: str) -> None:
+    assert eval(check)
+    text = """
+        from typing import List
+        def f():
+            assert {}
+    """
+    text = text.format(check)
+    theorem = prove_f(text)
+    assert theorem.conclusion is Conclusion.OK
 
 
 def test_list_append():
--- tests/test_types/test_set.py
+++ tests/test_types/test_set.py
@@ -1,5 +1,51 @@
+import pytest
 from deal_solver import Conclusion
 from ..helpers import prove_f
+
+
+@pytest.mark.parametrize('check', [
+    # compare
+    '{1, 2, 3} == {1, 2, 3}',
+    '{1, 2, 3} != {1, 2, 3, 4}',
+    '{1, 2, 3} == {3, 1, 2}',
+    '{1, 2, 3} == {3, 2, 1, 1, 2}',
+
+    # 'len({7, 9, 9, 9, 11}) == 3',
+
+    # compare
+    '10 in {3, 6, 10, 17}',
+    '2 not in {3, 6, 10, 17}',
+
+    # operations
+    '{1, 2} | {2, 3} == {1, 2, 3}',
+    '{1, 2} ^ {2, 3} == {1, 3}',
+    '{1, 2} & {2, 3} == {2}',
+
+    # methods
+    '{1, 2}.union({2, 3}) == {1, 2, 3}',
+    '{1, 2}.intersection({2, 3}) == {2}',
+    '{1, 2}.symmetric_difference({2, 3}) == {1, 3}',
+    '{1, 2}.difference({2, 3}) == {1}',
+    '{1, 2}.copy() == {1, 2}',
+
+    # is* methods
+    '{1, 2}.issubset({1, 2, 3})',
+    'not {1, 2, 3}.issubset({1, 2})',
+    '{1, 2, 3}.issuperset({1, 2})',
+    'not {1, 2}.issuperset({1, 2, 3})',
+    '{1, 2}.isdisjoint({3, 4})',
+    'not {1, 2}.isdisjoint({2, 3})',
+])
+def test_expr_asserts_ok(prefer_real: bool, check: str) -> None:
+    assert eval(check)
+    text = """
+        from typing import List
+        def f():
+            assert {}
+    """
+    text = text.format(check)
+    theorem = prove_f(text)
+    assert theorem.conclusion is Conclusion.OK
 
 
 def test_set_clear():
